56a6d7c8676a91c6712d1e64205c4d7841b3f056
Emídio Rebouças <emidioreb@gmail.com>
2021-07-03 11:04:47 -0300

    atualizando

---
+++ script.js
@@ -0,0 +1,27 @@
+const checkedStatus = document.getElementById('agreement');
+const button = document.getElementById('submit-btn');
+function statusButton() {
+  console.log(checkedStatus.checked);
+  if (checkedStatus.checked === true) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+}
+checkedStatus.addEventListener('click', statusButton);
+
+// Requito 3
+function submitButton() {
+  const buttonSubmit = document.getElementById('buttonSubmit');
+  buttonSubmit.addEventListener('click', () => {
+    const user = document.getElementById('user');
+    const password = document.getElementById('password');
+    if (user.value === 'tryber@teste.com' && password.value === '123456') {
+      alert('Olá, Tryber!');
+    }
+    if (user.value !== 'tryber@teste.com' || password.value !== '123456') {
+      alert('Login ou senha inválidos.');
+    }
+  });
+}
+submitButton();
